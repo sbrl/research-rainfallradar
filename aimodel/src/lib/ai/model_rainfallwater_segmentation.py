de47a883d9f294a751a221d5f1918fe6c978ab50
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2022-09-28 18:17:22 +0100

    missing units

--- aimodel/src/lib/ai/model_rainfallwater_segmentation.py
+++ aimodel/src/lib/ai/model_rainfallwater_segmentation.py
@@ -14,7 +14,7 @@ def model_rainfallwater_segmentation(metadata, feature_dim_in, shape_water_out, 
 	)
 	
 	# BEGIN
-	layer_next = tf.keras.layers.Dense(name="cns.stage.begin.dense")(layer_input)
+	layer_next = tf.keras.layers.Dense(name="cns.stage.begin.dense", units=feature_dim_in)(layer_input)
 	layer_next = tf.keras.layers.LayerNormalisation(name="stage_begin.norm", epsilon=1e-6)(layer_next)
 	layer_next = tf.keras.layers.ReLU(name="stage_begin.relu")(layer_next)
 	
