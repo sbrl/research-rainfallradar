bb0679a5094c578237d14c0df1ace1d1df71800d
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2022-10-25 21:11:48 +0100

    model_segmentation: don't softmax twice

--- aimodel/src/lib/ai/model_rainfallwater_segmentation.py
+++ aimodel/src/lib/ai/model_rainfallwater_segmentation.py
@@ -44,7 +44,7 @@ def model_rainfallwater_segmentation(metadata, shape_water_out, model_arch="conv
 	# TODO: An attention layer here instead of a dense layer, with a skip connection perhaps?
 	logger.warning("Warning: TODO implement attention from https://ieeexplore.ieee.org/document/9076883")
 	layer_next = tf.keras.layers.Dense(32)(layer_next)
-	layer_next = tf.keras.layers.Conv2D(water_bins, kernel_size=1, activation="softmax", padding="same")(layer_next)
+	layer_next = tf.keras.layers.Conv2D(water_bins, kernel_size=1, padding="same")(layer_next)
 	layer_next = tf.keras.layers.Softmax(axis=-1)(layer_next)
 	
 	model = tf.keras.Model(
