5f8d6dc6ea7b29446a6430855a24bc9d4a21dae1
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2022-10-31 19:26:10 +0000

    Add metrics every 64 batches this is important, because with large batches it can be difficult to tell what's happening inside each epoch.

--- aimodel/src/lib/ai/helpers/make_callbacks.py
+++ aimodel/src/lib/ai/helpers/make_callbacks.py
@@ -3,10 +3,12 @@ import os
 import tensorflow as tf
 
 from ..components.CallbackCustomModelCheckpoint import CallbackCustomModelCheckpoint
+from ..components.CallbackNBatchCsv import CallbackNBatchCsv
 
 def make_callbacks(dirpath, model_predict):
 	dirpath_checkpoints = os.path.join(dirpath, "checkpoints")
 	filepath_metrics = os.path.join(dirpath, "metrics.tsv")
+	filepath_metrics_batch = os.path.join(dirpath, "metrics_batch64.tsv")
 	
 	if not os.path.exists(dirpath_checkpoints):
 		os.mkdir(dirpath_checkpoints)
@@ -23,6 +25,10 @@ def make_callbacks(dirpath, model_predict):
 		tf.keras.callbacks.CSVLogger(
 			filename=filepath_metrics,
 			separator="\t"
+		),
+		CallbackNBatchCsv(
+			filepath=filepath_metrics_batch,
+			n_batches=64
 		),
 		tf.keras.callbacks.ProgbarLogger(count_mode="steps") # batches
 	]
