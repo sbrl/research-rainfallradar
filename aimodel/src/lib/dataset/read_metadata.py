0aa2ce19f5d7e58cffc0239e7091e52ef790585b
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2022-11-10 19:53:30 +0000

    read_metadata: support file inputs as well as dirs

--- aimodel/src/lib/dataset/read_metadata.py
+++ aimodel/src/lib/dataset/read_metadata.py
@@ -5,6 +5,9 @@ import json
 from ..io.readfile import readfile
 
 def read_metadata(dirpath_dataset):
-	filepath_metadata = os.path.join(dirpath_dataset, "metadata.json")
+	if os.path.isfile(dirpath_dataset):
+		filepath_metadata = os.path.join(os.path.dirname(dirpath_dataset), "metadata.jsonl")
+	else:
+		filepath_metadata = os.path.join(dirpath_dataset, "metadata.json")
 	
 	return json.loads(readfile(filepath_metadata))
