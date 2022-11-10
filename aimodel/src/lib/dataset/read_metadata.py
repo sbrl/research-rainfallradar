daf691bf435b843521a2cd0873a2f740434d8dc9
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2022-11-10 19:55:00 +0000

    typo

--- aimodel/src/lib/dataset/read_metadata.py
+++ aimodel/src/lib/dataset/read_metadata.py
@@ -6,7 +6,7 @@ from ..io.readfile import readfile
 
 def read_metadata(dirpath_dataset):
 	if os.path.isfile(dirpath_dataset):
-		filepath_metadata = os.path.join(os.path.dirname(dirpath_dataset), "metadata.jsonl")
+		filepath_metadata = os.path.join(os.path.dirname(dirpath_dataset), "metadata.json")
 	else:
 		filepath_metadata = os.path.join(dirpath_dataset, "metadata.json")
 	
