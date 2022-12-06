d0f2e3d730a5d141e97466a9bc67ec3f28f88bcc
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2022-12-06 19:27:39 +0000

    readfile: do transparent gzip by default ....but there's a glad to turn it off if needed

--- aimodel/src/lib/io/readfile.py
+++ aimodel/src/lib/io/readfile.py
@@ -1,7 +1,9 @@
 import io
 
-def readfile(filepath):
-	handle = io.open(filepath, "r")
+from .handle_open import handle_open
+
+def readfile(filepath, transparent_gzip=True):
+	handle = io.open(filepath, "r") if transparent_gzip else handle_open(filepath, "r")
 	content = handle.read()
 	handle.close()
 	return content
