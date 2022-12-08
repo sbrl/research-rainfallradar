ff56f591c79221292638e8116f8fb69d6300cd84
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2022-12-08 18:53:37 +0000

    I hate python

--- aimodel/src/lib/dataset/parse_heightmap.py
+++ aimodel/src/lib/dataset/parse_heightmap.py
@@ -6,7 +6,7 @@ from ..io.readfile import readfile
 def parse_heightmap(filepath_heightmap):
 	obj = json.loads(readfile(filepath_heightmap))
 	
-	result = tf.constant(obj.data)
+	result = tf.constant(obj["data"])
 	result = tf.transpose(result, [1,0]) # [ height, width ] → [ width, height ]
 	
 	return result
