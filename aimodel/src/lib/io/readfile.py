b53db648bfde4839b7d07b597e87118d12ff16bc
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2022-12-08 18:31:42 +0000

    fixup

--- aimodel/src/lib/io/readfile.py
+++ aimodel/src/lib/io/readfile.py
@@ -3,7 +3,7 @@ import io
 from .handle_open import handle_open
 
 def readfile(filepath, transparent_gzip=True):
-	handle = io.open(filepath, "r") if transparent_gzip else handle_open(filepath, "r")
+	handle = handle_open(filepath, "r") if transparent_gzip else io.open(filepath, "r")
 	content = handle.read()
 	handle.close()
 	return content
