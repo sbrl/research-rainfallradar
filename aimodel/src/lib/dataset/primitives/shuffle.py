20092c6829bb52f5d5a11791102664dae7bb79a1
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2023-05-11 15:59:01 +0100

    shuffle: add random seed env var

--- aimodel/src/lib/dataset/primitives/shuffle.py
+++ aimodel/src/lib/dataset/primitives/shuffle.py
@@ -1,5 +1,9 @@
+import os
+
 from copy import deepcopy
-from random import randint
+import random
+
+from loguru import logger
 
 
 def shuffle(lst):
@@ -9,10 +13,17 @@ def shuffle(lst):
 	@param	lst		list	The list to shuffle.
 	@return	list	The a new list that is a shuffled copy of the original.
 	"""
+
 	tmplist = deepcopy(lst)
 	m = len(tmplist)
+	
+	if RANDSEED in os.environ:
+		seed = os.environ["RANDSEED"]
+		random.seed(seed)
+		logger.info(f"Random seed set to {seed}, first 3 values: {random.randint(0, m)}, {random.randint(0, m)}, {random.randint(0, m)}")
+	
 	while (m):
 		m -= 1
-		i = randint(0, m)
+		i = random.randint(0, m)
 		tmplist[m], tmplist[i] = tmplist[i], tmplist[m]
 	return tmplist
