46265493529f2eac85c557af98cb7e684ea54b73
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2023-11-30 16:59:45 +0000

    rw/recompress: clarify subcommand description

--- rainfallwrangler/src/subcommands/recompress/meta.mjs
+++ rainfallwrangler/src/subcommands/recompress/meta.mjs
@@ -1,7 +1,7 @@
 "use strict";
 
 export default function(cli) {
-	cli.subcommand("recompress", "Recompress a source to a target directory with a given number of records per file.")
+	cli.subcommand("recompress", "Recompress a source .jsonl.gz to a target directory with a given number of records per file.")
 		.argument("source", "Path to the source directory.", null, "string")
 		.argument("target", "Path to the target directory.", null, "string")
 		.argument("count-file", "The number of records to store in each record file. See the documentation for the optimal value of this number (default: 4096).", 64*64, "integer");
