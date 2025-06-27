0abc0070f285f420132c982ad088511c9970d85d
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2025-06-27 18:19:53 +0100

    scripts/crossval-stbl: implement new form of metrics.tsv discovery

--- aimodel/scripts/crossval-stbl.py
+++ aimodel/scripts/crossval-stbl.py
@@ -31,19 +31,56 @@ epoch	metric_A	metric_B	…
 Environment variables:
 	ERROR_BAR_MODE=stddev
 		Statistic to use for error bars/area. Default: stddev. Possible values: stddev, mad
+	
+	MODE=strict|glob
+		How to search for metrics.tsv files. Possible values:
+		- `strict`: Pick all subdirectories of the target directory and assume they all contain a `metrics.tsv` file
+		- `glob`: Find all the files in the target directory that end in `metrics.tsv`
 """)
 	sys.exit(0)
 
 DIRPATH = sys.argv[1]  # [0] == script path
+
+MODE="strict"
+
+if "MODE" in os.environ:
+	MODE=os.environ["MODE"]
+
+supported_modes = [ "strict", "glob" ]
+if MODE not in supported_modes:
+	logger.error(f"Error: unrecognised mode {MODE}. Possible values: {", ".join(supported_modes)}")
+	sys.exit(2)
+
+logger.info(f"Operating in {MODE} mode")
 
 files = 0
 metrics = {}
 epochs = None
 
 for filepath in os.scandir(DIRPATH):
-	if not os.path.isdir(filepath):
+	filepath_metrics = None
+	match MODE:
+		case "strict":  # Pick only directories and assume they contain a metrics.tsv
+			if not os.path.isdir(filepath):
+				logger.info(f"REJECT/notadir: {filepath}")
+				continue
+			filepath_metrics = os.path.join(filepath, "metrics.tsv")
+		case "glob":  # take any files in the CURRENT directory ONLY (NO RECURSION) and keep if and only if it ends with metrics.tsv
+			if filepath.path.endswith("metrics.tsv"):
+				filepath_metrics = filepath.path
+		case _:
+			raise Exception(f"Error: Unrecognised mode {MODE}")
+
+	if filepath_metrics is None:
+		logger.info(f"REJECT/notpicked: {filepath.path}")
 		continue
-	tbl = pd.read_csv(os.path.join(filepath, "metrics.tsv"), sep="\t")
+
+	logger.info(f"READ {filepath_metrics}")
+	tbl = pd.read_csv(filepath_metrics, sep="\t")
+	
+	if tbl.columns.values[0] == "Unnamed: 0":
+		logger.warning("Fixing unnamed 1st column")
+		tbl.columns.values[0] = "epoch"
 
 	# metrics.append(tbl)
 
@@ -58,68 +95,74 @@ for filepath in os.scandir(DIRPATH):
 
 		metrics[column].append(tbl[column].values)
 		# print(column, tbl[column])
-	
+
 	# print("DEBUG:metrics", tbl)
 	files += 1
+
+
+if files == 0:
+	logger.error("Error: No metrics files detected, so can't plot graphs")
+	logger.error("Hint: Set the env var MODE to change how [*]metrics.tsv files are searched for.")
+	sys.exit(1)
 
 logger.info(f"Read {files} files into crossval-stbl{files} analysis")
 
 stats = {}
 
 for metric in metrics.keys():
-    metrics[metric] = pd.DataFrame(metrics[metric]).transpose()
+	metrics[metric] = pd.DataFrame(metrics[metric]).transpose()
 
-    if metric not in stats:
-        stats[metric] = {}
+	if metric not in stats:
+		stats[metric] = {}
 
-    stats[metric]["mad"] = scipy.stats.median_abs_deviation(
-        metrics[metric], axis=1
-    )  # median absolute deviation
-    stats[metric]["stddev"] = metrics[metric].std(axis=1)
-    stats[metric]["mean"] = metrics[metric].mean(axis=1)
-    stats[metric]["min"] = metrics[metric].min(axis=1)
-    stats[metric]["max"] = metrics[metric].max(axis=1)
-    stats[metric]["agg_min"] = stats[metric]["min"].min()
-    stats[metric]["agg_max"] = stats[metric]["max"].max()
-    stats[metric]["agg_stddev"] = metrics[metric].stack().std()
-    stats[metric]["agg_mean"] = metrics[metric].stack().std()
-    stats[metric]["agg_mad"] = scipy.stats.median_abs_deviation(
-        metrics[metric].stack()
-    )  # median absolute deviation
+	stats[metric]["mad"] = scipy.stats.median_abs_deviation(
+		metrics[metric], axis=1
+	)  # median absolute deviation
+	stats[metric]["stddev"] = metrics[metric].std(axis=1)
+	stats[metric]["mean"] = metrics[metric].mean(axis=1)
+	stats[metric]["min"] = metrics[metric].min(axis=1)
+	stats[metric]["max"] = metrics[metric].max(axis=1)
+	stats[metric]["agg_min"] = stats[metric]["min"].min()
+	stats[metric]["agg_max"] = stats[metric]["max"].max()
+	stats[metric]["agg_stddev"] = metrics[metric].stack().std()
+	stats[metric]["agg_mean"] = metrics[metric].stack().std()
+	stats[metric]["agg_mad"] = scipy.stats.median_abs_deviation(
+		metrics[metric].stack()
+	)  # median absolute deviation
 
-    # print(stats[metric])
+	# print(stats[metric])
 
-    plt.figure(figsize=(12, 8))
-    plt.ylim(min(0, stats[metric]["agg_min"]), max(1, stats[metric]["agg_max"]))
-    plt.grid(visible=True, which="major", axis="y", linewidth=2)
-    plt.grid(visible=True, which="minor", axis="y", linewidth=1)
-    plt.minorticks_on()
-    plt.rcParams['font.size'] = 32
-    plt.fill_between(
-        epochs,
-        stats[metric]["min"],
-        stats[metric]["max"],
-        alpha=0.2,
-        facecolor="#B7DE28",
-        edgecolor="#FDE724",
-        linestyle="dotted",
-        linewidth=1,
-    )
-    plt.fill_between(
-        epochs,
-        stats[metric]["mean"] - stats[metric][ERROR_BAR_MODE],
-        stats[metric]["mean"] + stats[metric][ERROR_BAR_MODE],
-        alpha=0.5,
-        facecolor="#228A8D",
-        edgecolor="#3CBB74",
-        linestyle="dashed",
-        linewidth=1,
-    )
-    plt.plot(epochs, stats[metric]["mean"], color="#450C54")
-    plt.title(f"{metric} // crossval-stbl{files}")
-    plt.xlabel("epoch")
-    plt.ylabel(metric)
-    plt.savefig(os.path.join(DIRPATH, f"crossval-stbl{files}_{metric}_{ERROR_BAR_MODE}.png"))
-    plt.close()
+	plt.figure(figsize=(12, 8))
+	plt.ylim(min(0, stats[metric]["agg_min"]), max(1, stats[metric]["agg_max"]))
+	plt.grid(visible=True, which="major", axis="y", linewidth=2)
+	plt.grid(visible=True, which="minor", axis="y", linewidth=1)
+	plt.minorticks_on()
+	plt.rcParams['font.size'] = 32
+	plt.fill_between(
+		epochs,
+		stats[metric]["min"],
+		stats[metric]["max"],
+		alpha=0.2,
+		facecolor="#B7DE28",
+		edgecolor="#FDE724",
+		linestyle="dotted",
+		linewidth=1,
+	)
+	plt.fill_between(
+		epochs,
+		stats[metric]["mean"] - stats[metric][ERROR_BAR_MODE],
+		stats[metric]["mean"] + stats[metric][ERROR_BAR_MODE],
+		alpha=0.5,
+		facecolor="#228A8D",
+		edgecolor="#3CBB74",
+		linestyle="dashed",
+		linewidth=1,
+	)
+	plt.plot(epochs, stats[metric]["mean"], color="#450C54")
+	plt.title(f"{metric} // crossval-stbl{files}")
+	plt.xlabel("epoch")
+	plt.ylabel(metric)
+	plt.savefig(os.path.join(DIRPATH, f"crossval-stbl{files}_{metric}_{ERROR_BAR_MODE}.png"))
+	plt.close()
 
 logger.success(f"Written {len(stats.keys())} graphs to {DIRPATH}")
